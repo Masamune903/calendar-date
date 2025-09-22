2add7cb6a8a6d0c7aab5e807c92d27e954843e11
Masamune903 <masamune_903@outlook.jp>
2025-09-22 21:55:19 +0900

    JSR公開用に不必要なファイルを削除

--- impls/ISOCalendarDate/ISOCalendar.example.ts
+++
@@ -1,9 +0,0 @@
-import { ISOCalendarDate } from "./ISOCalendarDate.ts";
-
-const date = ISOCalendarDate.from(730730);  // 2001年9月3日
-console.log(date.year.number);
-console.log(date.month.number);
-console.log(date.day.number);
-console.log(ISOCalendarDate.from(date))
-
-ISOCalendarDate.from([1, 1, 1]).dayIndex;
