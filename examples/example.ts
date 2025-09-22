89068b72556aa17055b43ddce70101f156f68cd2
Masamune903 <masamune_903@outlook.jp>
2025-09-22 23:06:05 +0900

    命名の変更を適用

--- examples/example.ts
+++ examples/example.ts
@@ -1,10 +1,10 @@
-// import { ISOCalendarDate, KyurekiCalendarDate } from "@amgm-masamune/japanese-date";
+import { ISOCalendarDate, KyurekiCalendarDate } from "@amgm-masamune/calendar-date";
 
-// // 2025年9月22日
-// const isoDate = ISOCalendarDate.from([2025, 9, 22]);
+// 2025年9月22日
+const isoDate = ISOCalendarDate.from([2025, 9, 22]);
 
-// // 旧2025年8月1日
-// const kyurekiDate  = KyurekiCalendarDate.from(isoDate);
-// console.log(kyurekiDate.toString());
-// console.log(kyurekiDate.rokuyo.name); // 先勝
-// console.log(kyurekiDate.month.alias);  // 葉月
+// 旧2025年8月1日
+const kyurekiDate  = KyurekiCalendarDate.from(isoDate);
+console.log(kyurekiDate.toString());
+console.log(kyurekiDate.rokuyo.name); // 先勝
+console.log(kyurekiDate.month.alias);  // 葉月
